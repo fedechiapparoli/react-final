e8ccd82c8b2fcd275ae2e10db96a31b4cbf74901
fedechiapparoli <fchiapparoli@yahoo.com.ar>
2021-12-14 01:15:41 -0300

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,15 @@ const navBar = () => {
         <div className='navBard'>
 
         <div className='navSide'>
+            <h3 className='logo'>Pericote Bebidas</h3>
         <div className='links'>
             <a href='/home'>Inicio</a>
             <a href='/Us'>Nosotros</a>
             <a href='/contact'>Contactos</a>
+            
+            
         </div>
+        
         <div className='rigthSide'>
             
         </div>
