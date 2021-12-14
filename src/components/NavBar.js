dca33031f60a2b91dd3d5347b3d6b761dc9a83e6
fedechiapparoli <fchiapparoli@yahoo.com.ar>
2021-12-13 23:20:10 -0300

    subida proyecto

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const navBar = () => {
+    return (
+        <div className='navBard'>
+
+        <div className='navSide'>
+        <div className='links'>
+            <a href='/home'>Inicio</a>
+            <a href='/Us'>Nosotros</a>
+            <a href='/contact'>Contactos</a>
+        </div>
+        <div className='rigthSide'>
+            
+        </div>
+            
+        </div>    
+       </div>
+    )
+
+}
+export default navBar
+
+
+
