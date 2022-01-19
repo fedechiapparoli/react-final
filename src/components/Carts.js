d688ad48f0053300a5773caa8b5e7f3287049b21
fedechiapparoli <fchiapparoli@yahoo.com.ar>
2022-01-19 10:52:03 -0300

    cart view 1

--- src/components/Carts.js
+++ src/components/Carts.js
@@ -3,46 +3,30 @@ import { CartContext } from "../Context/CartContext";
 
 const Carts = () => {
   const { cart, removeItem } = useContext(CartContext);
-  const [ total, setTotal ] = useState(0);
+  const [total, setTotal] = useState(0);
   const clickRemove = (id) => {
     removeItem(id);
   };
-  useEffect(
-    () => {
-      let totalCalculado = 0;
-      cart.forEach(item => {
-        totalCalculado += parseInt((item.price).slice(1)) * item.cantidad;
-      });
-      setTotal(totalCalculado);
-    },
-    [cart]
-  )
-
-
-​   return (
-  <div>
-    
-    {cart.map(i => 
-     
-          <div key={i.id}>
+  useEffect(() => {
+    let totalCalculado = 0;
+    cart.forEach((item) => {
+      totalCalculado += parseInt(item.price.slice(1)) * item.cantidad;
+    });
+    setTotal(totalCalculado);
+  }, [cart]);
+  return (
+    <div>
+      {cart.map((i) => (
+        <div key={i.id}>
           <img src={i.image} alt={i.name} />
-          <p>
-            Nombre:{i.name}
-          </p>
-          <p>
-            Precio:{i.price}
-          </p>
-          <p>
-            Cantidad: {i.cantidad}
-          </p>
+          <p> Nombre:{i.name} </p>
+          <p> Precio:{i.price}</p>
+          <p> Cantidad: {i.cantidad}</p>
           <button onClick={() => clickRemove(i.id)}>Eliminar</button>
-          <p>
-            Total: {total}
-          </p>
+          <p> Total: {total}</p>
         </div>
-        
-      )}
-  </div>
-)
-    }
+      ))}
+    </div>
+  );
+};
 export default Carts;
